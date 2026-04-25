ce414d2eaba8f44df07f9f025322ca1c8b4a476d
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-04-25 18:14:07 +0530

    Updated to hold size and time value

--- entry.h
+++ entry.h
@@ -1,11 +1,15 @@
 #ifndef ENTRY_H
 #define ENTRY_H
+#include <sys/types.h>
+#include <time.h>
 
 // Stores each item in a directory
 typedef struct
 {
     char name[256];
     unsigned char type;
+    off_t size;
+    time_t mtime;;
 } Entry;
 
 #endif
