339cfc57214271de9a7ef0d14ac9bf0a053ac591
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-04-26 21:32:20 +0530

    Auto regenaration of config file added if corrupted

--- config.c
+++ config.c
@@ -1,9 +1,11 @@
 #include "config.h"
+#include "colors.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
+// Loads config file
 Config load_config() {
     Config cfg = {1, 1};
     char config_path[512];
@@ -21,14 +23,47 @@ Config load_config() {
             fclose(new_config);
         }
     } else {
+        int icons_set = 0;
+        int color_set = 0;
+
         char line[128];
         while (fgets(line, sizeof(line), config)) {
-            if (!strncmp(line, "icons=false", 11))
+            if (!strncmp(line, "icons=true", 10)) {
+                cfg.icons = 1;
+                icons_set = 1;
+            }
+            if (!strncmp(line, "icons=false", 11)) {
                 cfg.icons = 0;
-            if (!strncmp(line, "color=false", 11))
+                icons_set = 1;
+            }
+            if (!strncmp(line, "color=true", 10)) {
+                cfg.color = 1;
+                color_set = 1;
+            }
+            if (!strncmp(line, "color=false", 11)) {
                 cfg.color = 0;
+                color_set = 1;
+            }
         }
         fclose(config);
+
+        if (!icons_set || !color_set) {
+            printf(CORAL "lsi: bad config, regenerating..." RESET "\n");
+            save_config(cfg);
+        }
     }
     return cfg;
+}
+
+// Saves a config
+void save_config(Config config) {
+    char config_path[512];
+    snprintf(config_path, sizeof(config_path), "%s/.config/lsi/lsi.conf", getenv("HOME"));
+
+    FILE *f = fopen(config_path, "w");
+    if (f != NULL) {
+        fprintf(f, "icons=%s\n", config.icons ? "true" : "false");
+        fprintf(f, "color=%s\n", config.color ? "true" : "false");
+        fclose(f);
+    }
 }
--- config.h
+++ config.h
@@ -1,11 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// Struct to hold config values
 typedef struct {
     int icons;
     int color;
 } Config;
 
+// Loads config file
 Config load_config();
+
+// Saves a config
+void save_config(Config config);
 
 #endif
