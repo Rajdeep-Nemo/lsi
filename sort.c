686cd5dc47dea73d034ab557f04fa8d8937fa579
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-05-16 09:02:35 +0530

    refactor: constant values are declared with const, and implicit conversions are replaced with explicit conversion

--- sort.c
+++ sort.c
@@ -1,4 +1,3 @@
-#define _DEFAULT_SOURCE
 #include "sort.h"
 #include "entry.h"
 #include <dirent.h>
@@ -8,8 +7,8 @@
 static int sort_by_global = SORT_NAME;
 
 int compare_entries(const void *a, const void *b) {
-    Entry *ea = (Entry *)a;
-    Entry *eb = (Entry *)b;
+    const Entry *ea = (Entry *)a;
+    const Entry *eb = (Entry *)b;
 
     // Directories first
     if (ea->type == DT_DIR && eb->type != DT_DIR)
@@ -19,23 +18,23 @@ int compare_entries(const void *a, const void *b) {
 
     // Sort by size
     if (sort_by_global == SORT_SIZE)
-        return ea->size - eb->size;
+        return (int)ea->size - (int)eb->size;
 
-    // Sort by last midified
+    // Sort by last modified
     if (sort_by_global == SORT_TIME)
-        return ea->mtime - eb->mtime;
+        return (int)ea->time - (int)eb->time;
 
     // Alphabetically
     return strcmp(ea->name, eb->name);
 }
 
-void sort_entries(Entry *entries, int count, int sort_by, int reverse) {
+void sort_entries(Entry *entries, const int count, const int sort_by, const int reverse) {
     sort_by_global = sort_by;
     qsort(entries, count, sizeof(Entry), compare_entries);
 
     if (reverse) {
         for (int i = 0; i < count / 2; i++) {
-            Entry temp = entries[i];
+            const Entry temp = entries[i];
             entries[i] = entries[count - 1 - i];
             entries[count - 1 - i] = temp;
         }
