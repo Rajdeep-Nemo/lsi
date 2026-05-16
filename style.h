fd5364224069e2c8a94ab228bc3e4104d91fe39e
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-05-16 09:19:41 +0530

    refactor: constant values are declared with const

--- style.h
+++ style.h
@@ -2,7 +2,7 @@
 #define STYLE_H
 #include "entry.h"
 
-char *getIcon(char *name, unsigned char type, mode_t mode);
-char *getColor(char *name, unsigned char type, mode_t mode);
+char *getIcon(const char *name, unsigned char type, mode_t mode);
+char *getColor(const char *name, unsigned char type, mode_t mode);
 
 #endif
