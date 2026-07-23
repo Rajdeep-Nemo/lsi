501be321f1dddf7224ec1adab02cb63ca30bf072
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-07-23 08:24:15 +0530

    Cleanup

--- detailed.h
+++
@@ -1,9 +0,0 @@
-#ifndef DETAILED_H
-#define DETAILED_H
-
-#include "config.h"
-#include "entry.h"
-
-void print_detailed(Entry *entries, int count, int max_len, Config config);
-
-#endif
--- style.h
+++
@@ -1,8 +0,0 @@
-#ifndef STYLE_H
-#define STYLE_H
-#include "entry.h"
-
-char *getIcon(const char *name, unsigned char type, mode_t mode);
-char *getColor(const char *name, unsigned char type, mode_t mode);
-
-#endif
