59c186cf51a689138f3d0dfe6b0a823242bcd387
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-07-24 21:39:43 +0530

    ...

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -24,3 +24,32 @@ func configPath() (string, error) {
 	return filepath.Join(configDir, "lsi", "lsi.conf"), nil
 }
 
+func initConfig() error {
+	// gets the path
+	path, err := configPath()
+	if err != nil {
+		return err
+	}
+	// Creates the config directory
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return err
+	}
+	// Create the config file if not present
+	if _, err := os.Stat(path); err == nil {
+		return nil
+	} else if os.IsNotExist(err) {
+		return err
+	}
+}
+
+func loadConfig() {
+
+}
+
+func handleConfigChange(key, value string) {
+	if value != "true" && value != "false" {
+		fmt.Fprintf(os.Stderr, "lsi: invalid value '%s' for --set-%s\n", value, key)
+		os.Exit(1)
+	}
+	// config.Save(...)
+}
