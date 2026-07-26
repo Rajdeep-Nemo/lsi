f09bee9b3d48a7944a73fd6bb4d9f8b978fa2fb7
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-07-26 10:00:07 +0530

    Commented and fixed initConfig()

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,17 +6,22 @@ import (
 	"path/filepath"
 )
 
-type config struct {
+// Struct to hold values
+type Config struct {
 	showIcons bool
 	showColor bool
 }
 
+// Default config to use
 const defaultConfig = `icons=true
 color=true
 `
 
 // Returns the path of the config file
 func configPath() (string, error) {
+	// Config Dir:
+	// Linux: /home/<username>/.config/lsi/lsi.conf
+	// MacOS: /Users/<username>/Library/Application Support/lsi/lsi.conf
 	configDir, err := os.UserConfigDir()
 	if err != nil {
 		return "", err
@@ -24,26 +29,28 @@ func configPath() (string, error) {
 	return filepath.Join(configDir, "lsi", "lsi.conf"), nil
 }
 
+// Initializes the config for the first time or if the file is corrupted
 func initConfig() error {
 	// gets the path
 	path, err := configPath()
 	if err != nil {
 		return err
 	}
-	// Creates the config directory
+	// Creates the config directory if not already
 	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 		return err
 	}
-	// Create the config file if not present
+	// If the file exist return nil
 	if _, err := os.Stat(path); err == nil {
 		return nil
-	} else if os.IsNotExist(err) {
-		return err
 	}
+	// If file doesn't exist, write defaultConfig
+	return os.WriteFile(path, []byte(defaultConfig), 0644)
 }
 
-func loadConfig() {
-
+// Reads and parses the config into the struct
+func loadConfig() (*Config, error){
+	
 }
 
 func handleConfigChange(key, value string) {
